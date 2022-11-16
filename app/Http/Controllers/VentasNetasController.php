ccbe1b0ef15f29e357f96d418aa812b105964eb2
Desarrollador web <ingenierodesarrollo@duquesa.com.co>
2022-11-15 19:21:25 -0500

    add fields to table ventas unit

--- app/Http/Controllers/VentasNetasController.php
+++ app/Http/Controllers/VentasNetasController.php
@@ -178,73 +178,76 @@ class VentasNetasController extends Controller
                 intval(round($divIndus)), intval(round($divAgAc)), intval(round($divServMaq)), intval(round($divTotlVen))
             ]);
             array_push($meses, ['mes' => $mes]);
-            $form = 0;
-            foreach ($dates as $form) {
-                $form = count($form);
+        }
+            array_push($meses, ['mes' => 'ACUMULADO']);
+            array_push($meses, ['mes' => 'PROMEDIO']);
+            $sumatoriasV=[];
+            foreach( $infoSales as $sums){
+                $aceites =  round($sums->ACEITES, 5);
+                $margarinas =  round($sums->MARGARINAS, 5);
+                $solidCre =  round($sums->SOLIDOS_CREMOSOS, 5);
+                $totPrTer= $aceites+$margarinas+$solidCre;
+                $industriales =  round($sums->INDUSTRIALES, 5);
+                $otros =  round($sums->ACIDOS_GRASOS_ACIDULADO, 5);
+                $serviMaqui =  round($sums->SERVICIO_MAQUILA, 5);
+                $totVen= $totPrTer+$industriales+$otros+$serviMaqui;
+                array_push($sumatoriasV,[$aceites,$margarinas,$solidCre,$totPrTer,$industriales,$otros,$serviMaqui,$totVen]);
             }
-        }
-        //dd($headers, $dates, $meses, $form);
-        //fin del generador del arreglo
-
-
-         foreach ($infoSales as $infoD) {
-            $aceiteUnit = round($infoD->ACEITES, 3);
-            $margaUnit = round($infoD->MARGARINAS, 3);
-            $solCrUnit = round($infoD->SOLIDOS_CREMOSOS, 3);
-            $totlPUnit = $aceiteUnit + $margaUnit + $solCrUnit;
-            $indusUnit = round($infoD->INDUSTRIALES, 3);
-            $aglAcid = round($infoD->ACIDOS_GRASOS_ACIDULADO, 3);
-            $servMaq = round($infoD->SERVICIO_MAQUILA, 3);
-            $totTBL = round(($totlPUnit + $indusUnit + $aglAcid + $servMaq) - $servMaq);
-            $dateObject = DateTime::createFromFormat('m', $infoD->INF_D_MES)->format('F');
-            array_push($infos, [$aceiteUnit, $margaUnit, $solCrUnit, $totlPUnit, $indusUnit, $aglAcid, $servMaq, $totTBL, $dateObject]);
-        }
-        //fin informacion
-        //informacion de vista ventas toneladas
-        foreach ($infoTons as $infoT) {
-            $aceiUnit2 = round($infoT->TON_ACEITES, 3);
-            $margaUnit2 = round($infoT->TON_MARGARINAS, 3);
-            $solCrUnit2 = round($infoT->TON_SOLIDOS_CREMOSOS, 3);
-            $totlPTUnit2 = $aceiUnit2 + $margaUnit2 + $solCrUnit2;
-            $indusUnit2 = round($infoT->TON_INDUSTRIALES_OLEO, 3);
-            $agAcid2 = round($infoT->TON_ACIDOS_GRASOS_ACIDULADO, 3);
-            $servMaq2 = round($infoT->TON_SERVICIO_MAQUILA, 3);
-            $totlVen2 = intval($totlPTUnit2) + intval($infoT->TON_INDUSTRIALES_OLEO) + intval(round($infoT->TON_ACIDOS_GRASOS_ACIDULADO));
-            array_push($infoTs, [$aceiUnit2, $margaUnit2, $solCrUnit2, $totlPTUnit2, $indusUnit2, $agAcid2, $servMaq2, $totlVen2]);
-        }
-        //fin de informacion
-        //contador de posiciones en arreglo de registros
-        $amount = count($infoTs);
-        //fin contador
-
-
-        
-        //repetidor para arreglo de informacion de vista
-        $prmediosOpUnit=[];
-        for ($i = 0; $i < $amount; $i++) {
-            $divAceit = $infos[$i][0] / $infoTs[$i][0];
-            $divMarga = $infos[$i][1] / $infoTs[$i][1];
-            $divSolCr = $infos[$i][2] / $infoTs[$i][2];
-            $divTotlPt = $infos[$i][3] / $infoTs[$i][3];
-            $divIndus = $infos[$i][4] / $infoTs[$i][4];
-            $divAgAc = $infos[$i][5] / $infoTs[$i][5];
-            $divServMaq = $infos[$i][6] / $infoTs[$i][6];
-            $divTotlVen = $infos[$i][7] / $infoTs[$i][7];
-            $mes = $infos[$i][8];
+            $sumatoriasT=[];
+            foreach( $infoTons as $sumst){
+                $aceites =  round($sumst->TON_ACEITES, 5);
+                $margarinas =  round($sumst->TON_MARGARINAS, 5);
+                $solidCre =  round($sumst->TON_SOLIDOS_CREMOSOS, 5);
+                $totPt= $aceites+$margarinas+$solidCre;
+                $industriales =  round($sumst->TON_INDUSTRIALES_OLEO, 5);
+                $otros =  round($sumst->TON_ACIDOS_GRASOS_ACIDULADO, 5);
+                $serviMaqui =  round($sumst->TON_SERVICIO_MAQUILA, 5);
+                $venTon= $totPt+$industriales+$otros;
+                array_push($sumatoriasT,[$aceites,$margarinas,$solidCre,$totPt,$industriales,$otros,$serviMaqui,$venTon]);
             }
-            $promedios = [];
-            $sumatorias = [];
-            for ($i = 0; $i < count($prmediosOperados[0]); $i++) {
-                $suma = 0;
-                foreach ($prmediosOperados as $prom) {
-                    $suma += $prom[$i];
+            $sumsV=[];
+            $promV=[];
+            for ($i = 0; $i < (count($sumatoriasV[0])); $i++) {
+            $suma = 0;
+                foreach ($sumatoriasV as $sum) {
+                $suma += $sum[$i];
                 }
-                array_push($promedios, [intval(round($suma / count($formates)))]);
-                array_push($sumatorias, [intval(round($suma))]);
+                $sumaV= $suma;
+                array_push($sumsV, intval(round($sumaV)));
+                array_push($promV, intval(round($sumaV))/count($infoSales));
+            };
+
+            $sumsT=[];
+            $promT=[];
+            for ($i = 0; $i < count($sumatoriasT[0]); $i++) {
+            $suma = 0;
+                foreach ($sumatoriasT as $sum) {
+                $suma += $sum[$i];
+                }
+                $sumaT= $suma;
+                array_push($sumsT, intval(round($sumaT)));
+                array_push($promT, intval(round($sumaT))/count($infoSales));
+            };
+
+
+            $acumulado=[];
+            for($i=0;$i<(count($sumsT)-1);$i++){
+                array_push($acumulado,$sumsV[$i]/$sumsT[$i]);
             }
-            array_push($formates,$sumatorias);
-            array_push($formates,$promedios);
-        
+            array_push($acumulado,($sumsV[7]-$sumsV[6])/$sumsT[7]);
+            array_push($dates,$acumulado);
+            
+            $promedio=[];
+            for($i=0;$i<(count($promV)-1);$i++){
+                array_push($promedio,$promV[$i]/$promT[$i]);
+            }
+            array_push($promedio,($promV[7]-$promV[6])/$promT[7]);
+            array_push($dates,$promedio);
+
+        $form = 0;
+        foreach ($dates as $form) {
+            $form = count($form);
+        }
             return view('SalesTotal\list_total_sales_unit', ['headers' => $headers, 'dates' => $dates, 'mes' => $meses, 'contador' => $form]);
         
         } 
